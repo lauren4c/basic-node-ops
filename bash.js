53a8d0183687d6f91e923d12bb91ae702ffa8d44
Lauren Forcey <lauren4c@gmail.com>
2019-02-14 16:09:50 -0700

    Completed Basic Node Operations Assignment.

---
+++ bash.js
@@ -0,0 +1,7 @@
+const commands = require("./commands.js");
+
+process.stdout.write("prompt > ");
+process.stdin.on("data", userInput => {
+  userInput = userInput.toString().trim();
+  commands.evaluateCmd(userInput);
+});
